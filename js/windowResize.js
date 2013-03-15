7f59e6f6563e15b027fc47d28218668717b16454
KITAO Kaoru <kero@KITAO-no-Mac-mini.local>
2013-03-16 01:07:11 +0900

    001.006

--- js/windowResize.js
+++ js/windowResize.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	var resize = function(){
+		var windowWidth = parseInt( $(window).width(), 10 );
 		var windowHeight = parseInt( $(window).height(), 10 );
 		var menuHeight = 100;
 		$( '#gmap' ).css({
 							'width' : '100%',
 							'height' : String(windowHeight - menuHeight) + 'px'
 						});
-		if ( parseInt( $(window).width(), 10 ) < 250 ) {
+		if ( windowWidth + windowHeight < 800 ) {
 			$( 'img' ).css({ 'width' : '31px', 'height' : '31px' });
 		} else {
 			$( 'img' ).css({ 'width' : '62px', 'height' : '62px' });
