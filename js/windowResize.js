36da6584f9caf345b6b2f556309e9ca1bb9ba6b1
KITAO Kaoru <kero@KITAO-no-Mac-mini.local>
2013-03-16 00:53:08 +0900

    001.004
    画像リサイズ機能追加

--- js/windowResize.js
+++ js/windowResize.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
 							'width' : '100%',
 							'height' : String(windowHeight - menuHeight) + 'px'
 						});
+		if ( parseInt( $(window).width(), 10 ) < 250 ) {
+			$( 'img' ).css({ 'width' : '31px', 'height' : '31px' });
+		} else {
+			$( 'img' ).css({ 'width' : '62px', 'height' : '62px' });
+		}
 	};
 	resize();
 	$( window ).resize( function(){
